99ac493b70a76ec845ed8d1716b2251f2d2f5e56
Jerry Lee <oldratlee@gmail.com>
2015-10-24 19:39:26 +0800

    test async

--- src/main/java/com/dubbo/demo/consumer/ConsumerMain.java
+++ src/main/java/com/dubbo/demo/consumer/ConsumerMain.java
@@ -1,21 +1,30 @@
 package com.dubbo.demo.consumer;
 
+import com.alibaba.dubbo.rpc.RpcContext;
 import com.dubbo.demo.HelloService;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.concurrent.Future;
 
 /**
  * @author Jerry Lee
  */
 public class ConsumerMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("application-context-consumer.xml");
         classPathXmlApplicationContext.start();
 
         HelloService helloService = (HelloService) classPathXmlApplicationContext.getBean("helloService");
-        String world = helloService.hello("World");
+
+        helloService.hello("World1");
+        Future<String> future1 = RpcContext.getContext().getFuture();
+
+        helloService.hello("World2");
+        Future<String> future2 = RpcContext.getContext().getFuture();
 
         System.out.println("=====================================");
-        System.out.println(world);
+        System.out.println(future1.get());
+        System.out.println(future2.get());
         System.out.println("=====================================");
     }
 }
--- src/main/java/com/dubbo/demo/impl/HelloServiceImpl.java
+++ src/main/java/com/dubbo/demo/impl/HelloServiceImpl.java
@@ -2,6 +2,8 @@ package com.dubbo.demo.impl;
 
 import com.dubbo.demo.HelloService;
 
+import java.util.Date;
+
 /**
  * @author Jerry Lee
  */
@@ -9,6 +11,12 @@ public class HelloServiceImpl implements HelloService {
     @Override
     public String hello(String name) {
         System.out.println("received " + name);
-        return "Hello, " + name + "!";
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return "Hello, " + name +  " " + System.currentTimeMillis() + "!";
     }
 }
