56bddfc7e13833ec1d27a1e0779bd0315aea3dfb
Jerry Lee <oldratlee@gmail.com>
2015-10-24 20:10:32 +0800

    update to use config api instead of xml config

--- src/main/java/com/dubbo/demo/consumer/ConsumerMain.java
+++ src/main/java/com/dubbo/demo/consumer/ConsumerMain.java
@@ -1,5 +1,7 @@
 package com.dubbo.demo.consumer;
 
+import com.alibaba.dubbo.config.ApplicationConfig;
+import com.alibaba.dubbo.config.ReferenceConfig;
 import com.alibaba.dubbo.rpc.RpcContext;
 import com.alibaba.dubbo.rpc.service.GenericService;
 import com.dubbo.demo.HelloService;
@@ -12,10 +14,20 @@ import java.util.concurrent.Future;
  */
 public class ConsumerMain {
     public static void main(String[] args) throws Exception {
-        ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("application-context-consumer.xml");
-        classPathXmlApplicationContext.start();
+        ReferenceConfig<GenericService> referenceConfig = new ReferenceConfig<GenericService>();
+        referenceConfig.setApplication(new ApplicationConfig("demo"));
+        referenceConfig.setInterface("com.dubbo.demo.HelloService");
+        referenceConfig.setVersion("1.0.0");
+        referenceConfig.setUrl("dubbo://127.0.0.1:20880");
+        referenceConfig.setAsync(true);
+        referenceConfig.setGeneric(true);
 
-        GenericService helloService = (GenericService) classPathXmlApplicationContext.getBean("helloService");
+
+//        ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("application-context-consumer.xml");
+//        classPathXmlApplicationContext.start();
+//
+//        GenericService helloService = (GenericService) classPathXmlApplicationContext.getBean("helloService");
+        GenericService helloService = referenceConfig.get();
 
         helloService.$invoke("hello", new String[]{"java.lang.String"}, new Object[]{"World1"});
         Future<String> future1 = RpcContext.getContext().getFuture();
